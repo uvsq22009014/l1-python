cec4f9fe588ad59732cad03c61996e2e373ab7b6
Maguelone de Besombes <maguelonedebesombes@MacBook-Air-de-Maguelone.local>
2020-10-23 10:34:54 +0200

    CM 23 octobre

---
+++ cours/05_outils/CM_23_oct.py
@@ -0,0 +1,26 @@
+# Quelques exemples de messages d'erreur
+
+# typo dans le nom de variable
+ma_variable = 7 
+ma_variable += 2
+
+# typo dans le nom de fonction
+def maFonction():
+    pass
+
+maFonction()
+
+# opération illégale 
+
+3 // 1
+
+
+# mauvais type
+x = 2
+if (x == 2):
+    print("Corrigez vos erreurs")
+
+# mauvaise indentation
+if(x > 1):
+    x +=1
+    print("décale moi")
---
+++ cours/05_outils/test_flake.py
@@ -0,0 +1,19 @@
+import math  # module pas utilisé
+
+
+def maFonction():
+    pass
+
+
+
+
+
+3+4*2  # instruction sans effet: pas détectée
+z = 2  # variable inutilisée: pas détectée
+
+
+x = 3
+y = 10
+while (x < y):
+    x += 1
+
